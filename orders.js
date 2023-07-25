041f02d9b278ea5eb81a3c520000e5f328173047
aditi2313 <guptaaditi2313@gmail.com>
2023-07-25 15:16:13 +0800

    Order quantity greater than 0.

--- orders.js
+++ orders.js
@@ -21,16 +21,23 @@ router.get('/:id', function(req, res) {
 
 // Post request to orders table.
 router.post('/', function(req, res) {
-    knex('orders').insert({
-        user_id: req.body.user_id,
-        order_details: req.body.order_details
-    })
-    .then(function() {
-        knex.select().from('orders')
-        .then(function(orders) {
-            res.send(orders);
+    const user_id = req.body.user_id;
+    const order_details = req.body.order_details;
+
+    if (order_details.quantity > 0) {
+        knex('orders').insert({
+            user_id: user_id,
+            order_details: order_details
         })
-    })
+        .then(function() {
+            knex.select().from('orders')
+            .then(function(orders) {
+                res.send(orders);
+            })
+        })
+    } else {
+        return res.status(401).json({ error: 'Invalid order details' });
+    }
 })
 
 // Delete request to delete an order.
